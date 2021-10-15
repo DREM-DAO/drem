ebddc983e676bf5c61d1f03d4d5a3c9a3c01de4c
Ludovit Scholtz <ludko2@gmail.com>
2021-10-15 21:34:45 +0200

    update

--- analysis/api/index.php
+++ analysis/api/index.php
@@ -3,11 +3,11 @@
 $files = scandir($dir = __DIR__ . '/../processes/');
 
 foreach($files as $file){
- $content = file_get_contents($dir.$file);
+  echo "<h2>$file</h2>";
+  $content = file_get_contents($dir.$file);
  $contentzip = gzcompress($content);
  $b64 = base64url_encode(substr($contentzip,2));
- echo "<h2>$file</h2>";
- echo "$b64"
+ echo "$b64";
 }
 
 
