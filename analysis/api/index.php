a9ad71cf8abdca8a2ebc5c0213781c35bcd27465
Ludovit Scholtz <ludko2@gmail.com>
2021-10-15 21:41:16 +0200

    2

--- analysis/api/index.php
+++ analysis/api/index.php
@@ -6,7 +6,7 @@ foreach($files as $file){
   echo "<h2>$file</h2>";
   $content = file_get_contents($dir.$file);
  $contentzip = zlib_encode($content, ZLIB_ENCODING_DEFLATE);
- $b64 = base64url_encode(substr($contentzip,1));
+ $b64 = base64url_encode(substr($contentzip,2));
  echo "$b64";
 }
 
