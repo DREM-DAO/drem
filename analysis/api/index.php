e911c28b9103f32ccb590bd00582cf9274221325
Ludovit Scholtz <ludko2@gmail.com>
2021-10-15 21:32:39 +0200

    update

--- analysis/api/index.php
+++ analysis/api/index.php
@@ -1,7 +1,16 @@
 <?php
 
-$files = scandir(__DIR__ . '/../processes/');
+$files = scandir($dir = __DIR__ . '/../processes/');
 
 foreach($files as $file){
- var_dump($file);
+ $content = file_get_contents($dir.$file);
+ $contentzip = gzcompress($content);
+ $b64 = base64url_encode(substr($contentzip,2));
+ echo "<h2>$file</h2>";
+ echo "$b64"
+}
+
+
+function base64url_encode( $data ){
+  return rtrim( strtr( base64_encode( $data ), '+/', '-_'), '=');
 }
