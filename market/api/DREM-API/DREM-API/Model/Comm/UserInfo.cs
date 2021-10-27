e09464edb6d21e8b7c53c2383727583951f1cfcb
Ludovit Scholtz <ludko2@gmail.com>
2021-10-27 08:42:28 +0200

    improve user/me endpoint .. return kyc verified, and isAdmin bool

---
+++ market/api/DREM-API/DREM-API/Model/Comm/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DREM_API.Model.Comm
+{
+    /// <summary>
+    /// Show user information
+    /// </summary>
+    public class UserInfo
+    {
+        /// <summary>
+        /// Crypto address
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// Is admin
+        /// </summary>
+        public bool IsAdmin { get; set; }
+        /// <summary>
+        /// is KYC verified
+        /// </summary>
+        public bool KYCVerified { get; set; }
+    }
+}
